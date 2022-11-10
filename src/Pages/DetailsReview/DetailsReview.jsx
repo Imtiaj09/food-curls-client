6667e359f97cb25d9d0f708fa3c9e6e2f9ae9736
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-11 00:37:40 +0600

    Review field has beed created

--- src/Pages/DetailsReview/DetailsReview.jsx
+++ src/Pages/DetailsReview/DetailsReview.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider/AuthProvider";
 
 const DetailsReview = () => {
-  const { img, price, title, description } = useLoaderData();
+  const { _id, img, price, title, description } = useLoaderData();
+  const { user } = useContext(AuthContext);
+
+  const handlePlaceReview = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const rating = form.rating.value;
+    const email = user?.email || "Unregister";
+    const message = form.message.value;
+
+    const reviews = {
+      review: _id,
+      name,
+      rating,
+      email,
+      message,
+    };
+  };
+
   return (
     <div>
       <div className="mb-6 overflow-hidden bg-white rounded-lg shadow-md">
@@ -22,6 +42,60 @@ const DetailsReview = () => {
             </a>
             <p className="mt-2 text-sm text-black dark:text-black">
               {description}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* REVIEW PART */}
+      <div className="hero">
+        <div className="hero-content flex-col lg:flex-row">
+          <form onSubmit={handlePlaceReview}>
+            <div className="ml-14 mb-6">
+              <h2 className="text-lg font-medium text-blue-600 uppercase dark:text-blue-400">
+                Your review matters!
+              </h2>
+              <p>How was your experience?</p>
+            </div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              className="mb-4 input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="text"
+              name="rating"
+              placeholder="Ratings"
+              className="mb-4 input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              defaultValue={user?.email}
+              className="mb-4 input input-bordered w-full max-w-xs"
+              readOnly
+            />
+            <br />
+            <textarea
+              className="textarea textarea-bordered h-24"
+              name="message"
+              placeholder="Leave a Review..."
+            ></textarea>
+            <br />
+            <input
+              className="btn btn-sm btn-outline btn-info"
+              type="submit"
+              value="Submit"
+            />
+          </form>
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
             </p>
           </div>
         </div>
--- src/Pages/Home/Services/ServicesCard.jsx
+++ src/Pages/Home/Services/ServicesCard.jsx
@@ -1,26 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const ServicesCard = ({ service }) => {
-  const { _id, img, price, title, description } = service;
+  const { img, price, title, description } = service;
   console.log(service);
   return (
-    <div class="max-w-xs overflow-hidden bg-white border rounded-lg shadow-lg ">
-      <div class="px-4 py-2">
-        <h1 class="text-lg font-bold text-gray-800 uppercase dark:text-black">
+    <div className="max-w-xs overflow-hidden bg-white border rounded-lg shadow-lg ">
+      <div className="px-4 py-2">
+        <h1 className="text-lg font-bold text-gray-800 uppercase dark:text-black">
           {title}
         </h1>
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+        <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
           {description.slice(0, 100) + "..."}
         </p>
       </div>
 
-      <img class="object-cover w-full h-48 mt-2" src={img} alt="" />
+      <img className="object-cover w-full h-48 mt-2" src={img} alt="" />
 
-      <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
-        <h1 class="text-lg font-bold text-white">Price: ${price}</h1>
+      <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
+        <h1 className="text-lg font-bold text-white">Price: ${price}</h1>
 
-        <button class="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
+        <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
           View Details
         </button>
       </div>
