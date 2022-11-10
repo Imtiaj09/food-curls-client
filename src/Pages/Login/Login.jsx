c54693bf7968fdab19b76a7404633ef8c97b07cb
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-10 12:12:50 +0600

    Login page has been created and firebase has been added with set envirment variable

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import img from "../../assets/images/login/login.svg";
 
 const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="hero w-full my-20">
       <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          <img className="w-3/4" src={img} alt="" />
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
+          <h1 className="text-5xl text-center font-bold">Login</h1>
+          <form onSubmit={handleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -40,9 +41,30 @@ const Login = () => {
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <input className="btn btn-primary" type="submit" value="Login" />
             </div>
-          </div>
+            <div className="flex items-center pt-4 space-x-1">
+              <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+              <p className="px-3 text-sm dark:text-gray-400">
+                Login with social accounts
+              </p>
+              <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+            </div>
+            <div className="flex justify-center space-x-4">
+              <button
+                aria-label="Log in with Google"
+                className="p-3 rounded-sm"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 32 32"
+                  className="w-5 h-5 fill-current"
+                >
+                  <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+                </svg>
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
